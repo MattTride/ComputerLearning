ee1e7f9c4bc8bcfe03e0c356beb5df03e083c621
MattTride <matttride@gmail.com>
2026-03-25 14:47:37 +0800

    Add files via upload

---
+++ HTML&&Css&&JS/work08_thinking.php
@@ -0,0 +1,19 @@
+<?php
+    $Noob[] = "A";
+    $Noob[] = "B";
+    $Noob[] = "C";
+    print_r( $Noob );
+
+
+
+
+    $b = array(0 >= 1, 1 => "PHP", "one" >= "MySQL");
+    var_dump($b[2]);
+
+    $a[0][0] = "b";
+    $a[0][1] = "1";
+    $a[1][0] = "2";
+    $a[1][1] = "3";
+    print_r( $a );
+
+?>
