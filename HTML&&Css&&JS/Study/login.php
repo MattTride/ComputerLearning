8451f815d4fab03ddba22134cfa3e9b6ae059841
MattTride <matttride@gmail.com>
2026-04-17 12:13:43 +0800

    Update login.php

--- HTML&&Css&&JS/Study/login.php
+++ HTML&&Css&&JS/Study/login.php
@@ -1,13 +1,18 @@
 <?php
-    $user = $_POST['username'];
-    $pass = $_POST['password'];
-    echo "拦截到的账号是:" . $user . "<br>";
+    $raw_user = $_POST['username'];
+    $raw_pass = $_POST['password'];
 
-    if($user == "admin" && $pass == "admin"){
-echo "<h2>登入成功!</h2>";}
-else{echo "<h2>登入失败</h2>";}
+    $clean_user = htmlspecialchars(trim($raw_user));
+    $clean_pass = trim($raw_pass);
 
+    if(strlen($clean_pass) < 6){
+        die("<h2>密码长度不足！</h2>>");
+    }
+    echo "后端安全拦截到的账号是" .$clean_user ."<br>";
 
-
-
+    if($clean_user == "admin" && $clean_pass == "admin666666"){
+        echo "<h2>登入成功，欢迎进入系统</h2>";
+    }else{
+        echo "<h2>账号密码错误！</h2>";
+    }
 ?>
