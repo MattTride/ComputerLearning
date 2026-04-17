abdf87262d967e4619c4a02108d0af6f1357bf6d
MattTride <matttride@gmail.com>
2026-04-17 13:45:13 +0800

    Update login.php

--- HTML&&Css&&JS/Study/login.php
+++ HTML&&Css&&JS/Study/login.php
@@ -1,4 +1,16 @@
 <?php
+    $host = "127.0.0.1";
+    $port = 8889;
+    $db_user = "root";
+    $db_pass = "root";
+    $db_name = "study_html";
+
+    $conn = mysqli_connect($host, $db_user, $db_pass, $db_name, $port);
+
+    if(!$conn){
+        die("数据库连接失败，MAMP开启了吗？账号密码对了吗?" .mysqli_connect_error());
+    }
+
     $raw_user = $_POST['username'];
     $raw_pass = $_POST['password'];
 
@@ -6,13 +18,16 @@
     $clean_pass = trim($raw_pass);
 
     if(strlen($clean_pass) < 6){
-        die("<h2>密码长度不足！</h2>>");
+        die("<h2>密码长度不足！</h2>");
     }
-    echo "后端安全拦截到的账号是" .$clean_user ."<br>";
 
-    if($clean_user == "admin" && $clean_pass == "admin666666"){
+
+    $sql = "select * from users where username = '$clean_user' and password = '$clean_pass'";
+    $result = mysqli_query($conn, $sql);
+    if(mysqli_num_rows($result) > 0){
         echo "<h2>登入成功，欢迎进入系统</h2>";
-    }else{
-        echo "<h2>账号密码错误！</h2>";
+    } else{
+        echo "<h2>账号密码错误</h2>";
     }
+    mysqli_close($conn);
 ?>
