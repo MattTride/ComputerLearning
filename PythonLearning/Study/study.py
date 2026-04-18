ed76a42c51e86061aeee4176792f319c8b9a4bc0
MattTride <matttride@gmail.com>
2026-04-18 13:44:17 +0800

    Add files via upload

---
+++ PythonLearning/Study/study.py
@@ -0,0 +1,21 @@
+hero_stat = ["Arthur", 5, 100]
+hero_stat[1] = 6
+hero_stat.append("红药水")
+print(hero_stat)
+
+enemies = ["哥布林", "史莱姆", "恶龙"]
+for item in enemies:
+    print(f"警告！前方出现{item}")
+
+
+def attack(target):
+    print(f"Arthur向{target}挥舞大剑！")
+    print(f"对{target}造成15点伤害")
+attack(enemies[0])
+
+def calculate_hp(current_hp, damage):
+    goblin_hp = current_hp - damage
+    return goblin_hp
+
+hp = calculate_hp(50, 15)
+print(f"{enemies[0]}剩余血量：{hp}")
